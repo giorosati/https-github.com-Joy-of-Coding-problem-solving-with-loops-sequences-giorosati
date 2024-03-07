9e197d6b94fd41cdd21e30803db891713c87726f
Giovanni <giovanni.rosati@gmail.com>
2024-03-07 12:40:29 -0600

    tests_passed

--- hello.py
+++ hello.py
@@ -3,17 +3,30 @@
 
 # Example
 def hello_world():
-    return "Hello!"
+    return "Hello World!"
 
 # 1
 def squared_sum(array):
-    # FIXME
-    return 0
+    sum = 0
+    for int in array:
+        sum += int * int
+    return sum
 
 # 2
 def mix(a, b):
-    # FIXME
-    return ""
+    combined_list = []
+    if len(a) >= len(b):
+        for i in range(len(a)):
+            combined_list.append(a[i])
+            if i < len(b):
+                combined_list.append(b[i])
+    else:
+        for i in range(len(b)):
+            if i < len(a):
+                combined_list.append(a[i])
+            combined_list.append(b[i])
+    combined_string = "".join(combined_list) 
+    return combined_string
 
 def main():
     print("squared sum: ", squared_sum([-3, 4]))
